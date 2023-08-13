86fb59c5eacb2fc81c3aa048538731fa034fc5d3
Keba87 <aleksandrkeba@gmail.com>
2023-08-13 18:58:48 +0300

    Added Specials card component and Specials sections

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,12 +5,12 @@ import Button from './ButtonMain';
 const CallToAction = () => {
     return (
         <Fragment>
-            <section>
+            <section className='callToActionSection'>
                 <div className='container'>
                     <div className='callToAction'>
-                        <h1>Little Lemon</h1>
-                        <h3>Chicago</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                        <h1 className='restName'>Little Lemon</h1>
+                        <h3 className='restCity'>Chicago</h3>
+                        <p className='restDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
                         <Button text='Reserve a table' onClickLink='/booking' />
                     </div>
                     <img className='heroImg' src={require('../assets/restauranfood.jpg')} alt='' />
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import CallToAction from '../components/CallToAction';
+import Specials from '../components/Specials';
 
 
 function HomePage() {
     return (
         <Fragment>
             <CallToAction />
+            <Specials />
         </Fragment>
 )}
 
